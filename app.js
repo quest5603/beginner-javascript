dc9880cdd817804eff42e24054171c9b6b5c1872
quest5603 <quest5602@gmail.com>
2017-02-23 15:31:29 -1000

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+console.log("hello")
+//alert("Sprinting toward the door the hapless chap bob trips")
+var x;
+
+x = 8;
+x = "bye";
+console.log(typeof x);
+console.log(x);
 function fortuneTeller(){
 
 }
